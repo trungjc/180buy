24268885826724efbdade7bf4b113507c6b6bb8d
unknown <trungnguyenthanh70@gmail.com>
2012-01-08 15:32:09 +0700

    day source len

---
+++ admin/config.php
@@ -0,0 +1,31 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost/180buy/admin/');
+define('HTTP_CATALOG', 'http://localhost/180buy/');
+define('HTTP_IMAGE', 'http://localhost/180buy/image/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost/180buy/admin/');
+define('HTTPS_IMAGE', 'http://localhost/180buy/image/');
+
+// DIR
+define('DIR_APPLICATION', 'D:\xampp\htdocs\180buy/admin/');
+define('DIR_SYSTEM', 'D:\xampp\htdocs\180buy/system/');
+define('DIR_DATABASE', 'D:\xampp\htdocs\180buy/system/database/');
+define('DIR_LANGUAGE', 'D:\xampp\htdocs\180buy/admin/language/');
+define('DIR_TEMPLATE', 'D:\xampp\htdocs\180buy/admin/view/template/');
+define('DIR_CONFIG', 'D:\xampp\htdocs\180buy/system/config/');
+define('DIR_IMAGE', 'D:\xampp\htdocs\180buy/image/');
+define('DIR_CACHE', 'D:\xampp\htdocs\180buy/system/cache/');
+define('DIR_DOWNLOAD', 'D:\xampp\htdocs\180buy/download/');
+define('DIR_LOGS', 'D:\xampp\htdocs\180buy/system/logs/');
+define('DIR_CATALOG', 'D:\xampp\htdocs\180buy/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysql');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', '180buy');
+define('DB_PREFIX', '');
+?>
---
+++ config.php
@@ -0,0 +1,30 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost/180buy/');
+define('HTTP_IMAGE', 'http://localhost/180buy/image/');
+define('HTTP_ADMIN', 'http://localhost/180buy/admin/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost/180buy/');
+define('HTTPS_IMAGE', 'http://localhost/180buy/image/');
+
+// DIR
+define('DIR_APPLICATION', 'D:\xampp\htdocs\180buy/catalog/');
+define('DIR_SYSTEM', 'D:\xampp\htdocs\180buy/system/');
+define('DIR_DATABASE', 'D:\xampp\htdocs\180buy/system/database/');
+define('DIR_LANGUAGE', 'D:\xampp\htdocs\180buy/catalog/language/');
+define('DIR_TEMPLATE', 'D:\xampp\htdocs\180buy/catalog/view/theme/');
+define('DIR_CONFIG', 'D:\xampp\htdocs\180buy/system/config/');
+define('DIR_IMAGE', 'D:\xampp\htdocs\180buy/image/');
+define('DIR_CACHE', 'D:\xampp\htdocs\180buy/system/cache/');
+define('DIR_DOWNLOAD', 'D:\xampp\htdocs\180buy/download/');
+define('DIR_LOGS', 'D:\xampp\htdocs\180buy/system/logs/');
+
+// DB
+define('DB_DRIVER', 'mysql');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', '180buy');
+define('DB_PREFIX', '');
+?>
